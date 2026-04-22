dda42563584776d6ecf812e8a47bcfb91acbec73
Mattia Cavina <167676876+Cav95@users.noreply.github.com>
2026-04-22 13:35:11 +0200

    To be fix user sutetication

--- Android/app/src/main/java/com/example/cacciaaltesoro/ui/database/Supabase.kt
+++ Android/app/src/main/java/com/example/cacciaaltesoro/ui/database/Supabase.kt
@@ -1,14 +1,47 @@
 package com.example.cacciaaltesoro.ui.database
 
+import android.provider.ContactsContract
+import io.github.jan.supabase.annotations.SupabaseInternal
 import io.github.jan.supabase.auth.Auth
+import io.github.jan.supabase.auth.OtpType
+import io.github.jan.supabase.auth.auth
+import io.github.jan.supabase.auth.providers.builtin.Email
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.postgrest.Postgrest
+import io.github.jan.supabase.toJsonObject
+import kotlinx.serialization.json.buildJsonObject
 
 object Supabase {
     val supabase = createSupabaseClient(
         supabaseUrl = "https://dkgltvsqbhtxfyywexlw.supabase.co",
         supabaseKey = "sb_publishable_G8rS-8Q792RWbbTQB3cMUQ_D7lUQHcY" // Ensure this is your correct 'anon' or 'service' key
     ) {
-        install(Postgrest)
+        install(Postgrest);
+        install(Auth);
+    }
+
+
+    @OptIn(SupabaseInternal::class)
+    suspend public fun signUp(email: String, pass: String, username: String) {
+        supabase.auth.signUpWith(Email) {
+            this.email = email
+            this.password = pass
+            // Salviamo lo username nei metadati dell'utente Auth
+            data = buildJsonObject {
+                put("username", username.toJsonObject())
+            }
+        }
+    }
+
+    suspend public fun login(email: String, pass: String) {
+        try {
+            supabase.auth.signInWith(Email) {
+                this.email = email
+                this.password = pass
+            }
+            println("Login effettuato!")
+        } catch (e: Exception) {
+            println("Errore: ${e.message}")
+        }
     }
 }
--- Android/app/src/test/java/com/example/cacciaaltesoro/ConnectionUnitTest.kt
+++ Android/app/src/test/java/com/example/cacciaaltesoro/ConnectionUnitTest.kt
@@ -3,6 +3,8 @@ package com.example.cacciaaltesoro
 import com.example.cacciaaltesoro.ui.database.Supabase
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.auth.auth
+import junit.framework.AssertionFailedError
+import kotlinx.coroutines.test.runTest
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -17,5 +19,22 @@ class ConnectionUnitTest {
         val conn = Supabase.supabase;
 
         assertEquals("sb_publishable_G8rS-8Q792RWbbTQB3cMUQ_D7lUQHcY",conn.supabaseKey)
+    }
+
+    @Test
+    fun `test login` () = runTest{
+        val conn = Supabase.supabase;
+        val email = "mattia.cavina2@studio.unibo.it"
+        val username = email
+        val password = "pwd123"
+
+        try {
+            Supabase.login(email,password)
+        }
+        catch ( e: Exception){
+            assertEquals(1,2)
+        }
+
+
     }
 }
