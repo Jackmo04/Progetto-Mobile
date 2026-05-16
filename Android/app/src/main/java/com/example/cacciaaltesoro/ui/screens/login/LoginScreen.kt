f3dd9f882a8760f0f69cd3357c31d8daaefeea5a
Mattia Cavina <167676876+Cav95@users.noreply.github.com>
2026-05-16 11:35:59 +0200

    Add Material3 Snackbar to LoginScreen
    Replace Toast with Snackbar for camera permission notification and update version catalog.

--- Android/app/src/main/java/com/example/cacciaaltesoro/ui/screens/login/LoginScreen.kt
+++ Android/app/src/main/java/com/example/cacciaaltesoro/ui/screens/login/LoginScreen.kt
@@ -4,7 +4,6 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.util.Log
-import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
@@ -26,7 +25,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -55,6 +53,7 @@ fun LoginScreen(
     val context = LocalContext.current
     val contentResolver = context.contentResolver
     val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
 
     val state by viewModel.state.collectAsStateWithLifecycle()
     val isUpdatePassword = state.isUpdatePassword
@@ -111,8 +110,9 @@ fun LoginScreen(
         if (isGranted) {
             takePicture()
         } else {
-            Toast.makeText(context, "Permesso fotocamera negato", Toast.LENGTH_SHORT).show()
-        }
+            scope.launch {
+            snackbarHostState.showSnackbar("Permesso di uso forocamera necessario!")
+        }}
     }
 
     fun checkAndRequestCameraPermission() {
@@ -124,7 +124,8 @@ fun LoginScreen(
     }
 
     Scaffold(
-        topBar = { AppBar(title, navController) }
+        topBar = { AppBar(title, navController) },
+        snackbarHost = { SnackbarHost(snackbarHostState) }
     ) { contentPadding ->
 
         if (state.isInitializing) {
