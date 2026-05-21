da3eaaeef290c11de4009e0c08a0fb89cb656d44
Mattia Cavina <mattiacavina95@gmail.com>
2026-05-21 13:59:57 +0200

    Update Compose BOM and Material3 dependencies in version catalog

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -46,11 +46,10 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    //implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation("androidx.compose.material3:material3:1.4.0")
+    implementation(libs.androidx.compose.material3.v140)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.ktor.serialization.kotlinx.json)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
@@ -74,7 +73,7 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     implementation(libs.google.maps.services)
     implementation(libs.androidx.core.splashscreen)
-    implementation(platform("androidx.compose:compose-bom:2026.05.01"))
+    implementation(platform(libs.androidx.compose.bom.v20260501))
 
     //Supbase dependence
     implementation(platform(libs.bom.v300))
