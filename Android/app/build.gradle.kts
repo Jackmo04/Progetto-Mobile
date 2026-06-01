69a7bfc2b9370ec08ffff99c67945d6387dfb4f9
Matteo Grandini <matteo.grandini@studio.unibo.it>
2026-06-01 15:20:59 +0200

    More warning fixes

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -16,6 +16,7 @@ android {
     defaultConfig {
         applicationId = "com.example.cacciaaltesoro"
         minSdk = 34
+        //noinspection OldTargetApi
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
--- Android/app/src/main/java/com/example/cacciaaltesoro/utils/Image.kt
+++ Android/app/src/main/java/com/example/cacciaaltesoro/utils/Image.kt
@@ -11,37 +11,6 @@ import android.provider.MediaStore
 import java.io.FileNotFoundException
 
 fun uriToBitmap(imageUri: Uri, contentResolver: ContentResolver): Bitmap {
-    val bitmap = when {
-        Build.VERSION.SDK_INT < 28 -> {
-            @Suppress("DEPRECATION")
-            MediaStore.Images.Media.getBitmap(contentResolver, imageUri)
-        }
-        else -> {
-            val source = ImageDecoder.createSource(contentResolver, imageUri)
-            ImageDecoder.decodeBitmap(source)
-        }
-    }
-    return bitmap
-}
-
-fun saveImageToStorage(
-    imageUri: Uri,
-    contentResolver: ContentResolver,
-    name: String = "IMG_${SystemClock.uptimeMillis()}"
-): Uri {
-    val bitmap = uriToBitmap(imageUri, contentResolver)
-
-    val values = ContentValues()
-    values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
-    values.put(MediaStore.Images.Media.DISPLAY_NAME, name)
-
-    val savedImageUri =
-        contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
-    val outputStream = savedImageUri?.let { contentResolver.openOutputStream(it) }
-        ?: throw FileNotFoundException()
-
-    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
-    outputStream.close()
-
-    return savedImageUri
+    val source = ImageDecoder.createSource(contentResolver, imageUri)
+    return ImageDecoder.decodeBitmap(source)
 }
